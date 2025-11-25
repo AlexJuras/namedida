daa4da5bc6158687537f36d0738e8df365f909e7
Alex Victor <alexvictorphb@gmail.com>
2025-11-24 22:05:31 -0300

    O primeiro commit

---
+++ app/Models/Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClienteFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'telefone',
+        'endereco',
+    ];
+
+    // public function alugueis()
+    // {
+    //     return $this->hasMany(Aluguel::class);
+    // }
+}
